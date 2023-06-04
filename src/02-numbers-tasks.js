df8b495bbfbd046e16141ebf541893a449abb565
Anvar <fort2652@gmail.com>
2023-06-05 00:02:06 +0300

    feat: numbers

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -251,8 +251,13 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  // throw new Error('Not implemented');
+  const out = parseInt(value, 10);
+  if (Number.isNaN(out)) {
+    return def;
+  }
+  return out;
 }
 
 module.exports = {
